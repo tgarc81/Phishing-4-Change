76e9e3dbbb03da40126e86f35edfa27d2c2ef70c
Noah Quintin <naq1363@g.rit.edu>
2022-11-16 22:10:07 -0500

    Edited Collision

--- P4C/Assets/Scripts/Movement.cs
+++ P4C/Assets/Scripts/Movement.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    public float movementSpeed = 5;
+    public float movementSpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,26 +19,26 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey (KeyCode.LeftShift) && Input.GetKey("w")) 
         {
-            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed * 5f;
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed * 1.8f;
         } 
 
         else if (Input.GetKey("w") && !Input.GetKey (KeyCode.LeftShift))
         {
-            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed * 5f;
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed * 1.8f;
         }
 
         else if (Input.GetKey ("s"))
         {
-            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed * 5f;
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed * 1.8f;
         }
 
         if(Input.GetKey("a") && !Input.GetKey ("d")) 
         {
-            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 5f;
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 1.8f;
         }
         else if (Input.GetKey ("d") && !Input.GetKey ("a"))
         {
-            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 5f;
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 1.8f;
         }
     }
 }
